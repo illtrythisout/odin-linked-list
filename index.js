2e4c029b3228bb16c2fcc4db6ef67ba02986eb21
Theo <170449321+illtrythisout@users.noreply.github.com>
2024-11-19 14:57:45 -0300

    Create append function in LinkedList class

--- index.js
+++ index.js
@@ -10,4 +10,26 @@ class LinkedList {
     this.tail = null;
     this.size = 0;
   }
+
+  append(value) {
+    let currentNode = this.head;
+    const node = new Node(value);
+
+    if (!this.head) {
+      this.head = node;
+    } else {
+      while (currentNode.next) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = node;
+    }
+
+    this.size++;
+  }
 }
+
+// let li = new LinkedList();
+// li.append(1);
+// li.append(2);
+// li.append(3);
+// console.log(li);
